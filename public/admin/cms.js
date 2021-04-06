4e30bf049143dbd495d204ce137419f928126146
Abbariao <ABBARR01@pfizer.com>
2021-04-05 22:18:54 -0400

    updated

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,8 +1,8 @@
 
 import AboutPreview from "./templates/about.js";
 
-CMS.registerPreviewStyle("/css/resume.css");
-CMS.registerPreviewStyle("/css/tweaks.css");
+CMS.registerPreviewStyle("./css/resume.css");
+CMS.registerPreviewStyle("./css/tweaks.css");
 
 CMS.registerPreviewTemplate("about", AboutPreview);
 CMS.init();
